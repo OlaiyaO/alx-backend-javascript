0b00800c3a0064b3be8e5b1d7ea72a57e008cc0c
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-07-24 04:56:04 +0100

    testing my promise directory

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+function handleResponseFromAPI(promise) {
+  const body = { status: 200, body: 'success' };
+
+  return promise
+    .then(() => body)
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
+}
+export default handleResponseFromAPI;
---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+  let res;
+  try {
+    res = mathFunction();
+  } catch (error) {
+    res = `Error: ${error.message}`;
+  }
+  queue.push(res);
+  queue.push('Guardrail was processed');
+
+  return queue;
+}
